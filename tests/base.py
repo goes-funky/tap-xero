008248ec737c407e94f98b768f0707cc04acf305
Marsel Beqiri <marselbeqiri.365@gmail.com>
2021-09-16 15:09:16 +0200

    refactor: optimize and rearrange code

--- tests/base.py
+++ tests/base.py
@@ -10,8 +10,12 @@ from singer import utils
 def preserve_refresh_token(existing_conns, payload):
     if not existing_conns:
         return payload
-    conn_with_creds = connections.fetch_existing_connection_with_creds(existing_conns[0]['id'])
-    payload['properties']['refresh_token'] = conn_with_creds['credentials']['refresh_token']
+    conn_with_creds = connections.fetch_existing_connection_with_creds(
+        existing_conns[0]["id"]
+    )
+    payload["properties"]["refresh_token"] = conn_with_creds["credentials"][
+        "refresh_token"
+    ]
     return payload
 
 
@@ -21,13 +25,13 @@ class XeroScenarioBase(unittest.TestCase):
 
     def setUp(self):
         required_creds = {
-            "client_id": 'TAP_XERO_CLIENT_ID',
-            "client_secret": 'TAP_XERO_CLIENT_SECRET',
-            "refresh_token": 'TAP_XERO_REFRESH_TOKEN',
+            "client_id": "TAP_XERO_CLIENT_ID",
+            "client_secret": "TAP_XERO_CLIENT_SECRET",
+            "refresh_token": "TAP_XERO_REFRESH_TOKEN",
         }
         required_props = {
-            "tenant_id": 'TAP_XERO_TENANT_ID',
-            "xero_user_id": 'TAP_XERO_USER_ID'
+            "tenant_id": "TAP_XERO_TENANT_ID",
+            "xero_user_id": "TAP_XERO_USER_ID",
         }
         missing_creds = [v for v in required_creds.values() if not os.getenv(v)]
         missing_props = [v for v in required_props.values() if not os.getenv(v)]
@@ -35,15 +39,17 @@ class XeroScenarioBase(unittest.TestCase):
             missing_envs = missing_creds + missing_props
             raise Exception("set " + ", ".join(missing_envs))
         self._credentials = {k: os.getenv(v) for k, v in required_creds.items()}
-        self.conn_id = connections.ensure_connection(self, payload_hook=preserve_refresh_token)
+        self.conn_id = connections.ensure_connection(
+            self, payload_hook=preserve_refresh_token
+        )
 
     def get_type(self):
         return "platform.xero"
 
     def get_credentials(self):
-        self._credentials["client_secret"] = os.getenv('TAP_XERO_CLIENT_SECRET')
-        self._credentials["client_id"] = os.getenv('TAP_XERO_CLIENT_ID')
-        self._credentials["refresh_token"] = os.getenv('TAP_XERO_REFRESH_TOKEN')
+        self._credentials["client_secret"] = os.getenv("TAP_XERO_CLIENT_SECRET")
+        self._credentials["client_id"] = os.getenv("TAP_XERO_CLIENT_ID")
+        self._credentials["refresh_token"] = os.getenv("TAP_XERO_REFRESH_TOKEN")
         self._credentials["access_token"] = "access_token"
         return self._credentials
 
@@ -103,7 +109,7 @@ class XeroScenarioBase(unittest.TestCase):
             "receipts": ["UpdatedDateUTC"],
             "users": ["UpdatedDateUTC"],
             "linked_transactions": ["UpdatedDateUTC"],
-            "quotes": ["UpdatedDateUTC"]
+            "quotes": ["UpdatedDateUTC"],
         }
 
     @property
@@ -133,8 +139,8 @@ class XeroScenarioBase(unittest.TestCase):
     def get_properties(self):
         return {
             "start_date": self.start_dt.strftime("%Y-%m-%dT%H:%M:%SZ"),
-            "tenant_id": os.getenv('TAP_XERO_TENANT_ID'),
-            "xero_user_id": os.getenv('TAP_XERO_USER_ID'),
+            "tenant_id": os.getenv("TAP_XERO_TENANT_ID"),
+            "xero_user_id": os.getenv("TAP_XERO_USER_ID"),
         }
 
     def get_bookmark_default(self, stream):
@@ -148,15 +154,17 @@ class XeroScenarioBase(unittest.TestCase):
                 return int(bookmark_val)
             return utils.strptime_with_tz(bookmark_val)
         except Exception as e:
-            raise Exception("Couldn't cast (stream:{}) (bk_name:{}) (bk_val:{})"
-                            .format(stream, bookmark_name, bookmark_val)) from e
+            raise Exception(
+                "Couldn't cast (stream:{}) (bk_name:{}) (bk_val:{})".format(
+                    stream, bookmark_name, bookmark_val
+                )
+            ) from e
 
     def check_all_streams_in_catalogs(self, found_catalogs):
         found_catalog_names = {c["tap_stream_id"] for c in found_catalogs}
         diff = self.expected_streams.symmetric_difference(found_catalog_names)
         self.assertEqual(
-            len(diff), 0,
-            msg="discovered schemas do not match: {}".format(diff)
+            len(diff), 0, msg="discovered schemas do not match: {}".format(diff)
         )
         print("discovered schemas are kosher")
 
@@ -164,34 +172,44 @@ class XeroScenarioBase(unittest.TestCase):
         # selected = [menagerie.select_catalog(self.conn_id, c) for c in found_catalogs]
         # menagerie.post_annotated_catalogs(self.conn_id, selected)
         for catalog in found_catalogs:
-            schema = menagerie.get_annotated_schema(self.conn_id, catalog['stream_id'])
+            schema = menagerie.get_annotated_schema(self.conn_id, catalog["stream_id"])
             non_selected_properties = []
             additional_md = []
 
             connections.select_catalog_and_fields_via_metadata(
-                self.conn_id, catalog, schema, additional_md=additional_md,
-                non_selected_fields=non_selected_properties
+                self.conn_id,
+                catalog,
+                schema,
+                additional_md=additional_md,
+                non_selected_fields=non_selected_properties,
             )
 
     def select_specific_catalog(self, found_catalogs, catalog_to_select):
         for catalog in found_catalogs:
-            if catalog['tap_stream_id'] != catalog_to_select:
+            if catalog["tap_stream_id"] != catalog_to_select:
                 continue
 
-            schema = menagerie.get_annotated_schema(self.conn_id, catalog['stream_id'])
+            schema = menagerie.get_annotated_schema(self.conn_id, catalog["stream_id"])
             non_selected_properties = []
             additional_md = []
 
             connections.select_catalog_and_fields_via_metadata(
-                self.conn_id, catalog, schema, additional_md=additional_md,
-                non_selected_fields=non_selected_properties
+                self.conn_id,
+                catalog,
+                schema,
+                additional_md=additional_md,
+                non_selected_fields=non_selected_properties,
             )
             break
 
     def look_for_unexpected_bookmarks(self, bookmarks):
         diff = set(bookmarks).difference(self.expected_bookmarks)
         self.assertEqual(
-            len(diff), 0,
-            msg=("Unexpected bookmarks: {} Expected: {} Actual: {}"
-                 .format(diff, self.expected_bookmarks, bookmarks))
+            len(diff),
+            0,
+            msg=(
+                "Unexpected bookmarks: {} Expected: {} Actual: {}".format(
+                    diff, self.expected_bookmarks, bookmarks
+                )
+            ),
         )
